806b3fb747318e8608eb665d3e1ff34e108cdcb1
BonePolk <87945722+BonePolk@users.noreply.github.com>
2023-02-13 21:40:45 +0500

    chat filter and Caps on edit messages

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 from pyrogram import Client, filters
 
-app_id = ""
-app_hash = ""
+app_id = "28091304"
+app_hash = "bf3863212c54af7a45f20a5e0f8db84e"
 
 app = Client("my_account", app_id, app_hash)
 
 @app.on_message(filters.me)
 async def mes(client, message):
-    mes_id = message.id
-    message_text = message.text.upper()
-    chat_id = message.chat.id
+    return await editup(message)
+
+@app.on_edited_message(filters.me)
+async def edit(client, message):
+    return await editup(message)
+        
+async def editup(message):
+    print("yes")
+    chat_id = -1
     
-    await app.edit_message_text(chat_id, mes_id, message_text)
+    if (message.chat.id==chat_id):
+        mes_id = message.id
+        message_text = message.text.upper()
+        await app.edit_message_text(chat_id, mes_id, message_text)
+    
+    return
 
 app.run()
